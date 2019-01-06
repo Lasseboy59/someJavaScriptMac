b75c86d8d95b15fc377c45a447ed6423a19222cb
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-01-06 13:56:49 +0200

    using getTime() and timestamp in notes-app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,4 +1,4 @@
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
     searchText: ''
@@ -25,3 +25,44 @@ document.querySelector('#search-text').addEventListener('input', function(e){
 document.querySelector('#filter-by').addEventListener('change', function(e){
     console.log(e.target.value)
 })
+
+window.addEventListener('storage', function(e){
+
+    // debugger
+    // console.log('clicked')
+
+    if(e.key === 'notes'){
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes, filters)
+    }
+})
+
+// Unix Epoch - January 1st 1970 00:00:00 // -60000 (minute before)
+// const now = new Date('January 21 2001 6:25:01')
+
+const now = new Date()
+const timestamp = now.getTime()
+// console.log(timestamp)
+
+const myDate = new Date(timestamp)
+console.log(myDate.getFullYear())
+
+// console.log(now.getTime())
+
+// console.log(`Year: ${now.getFullYear()}`)
+// console.log(`Month: ${now.getMonth()}`)
+// console.log(`Day of the month: ${now.getDate()}`)
+// console.log(`Hour: ${now.getHours()}`)
+// console.log(`Minutes: ${now.getMinutes()}`)
+// console.log(`Seconds: ${now.getSeconds()}`)
+
+ const dayOne = new Date('January 21 2008 16:05:00')
+ const timestampOne = dayOne.getTime()
+ const dayTwo = new Date('January 21 2008 16:05:00')
+ const timestampTwo = dayTwo.getTime()
+
+ if(timestampOne < timestampTwo){
+     console.log(dayOne.toString())
+} else {
+     console.log(dayTwo.toString())
+ }
