565e597e7808d71e820a73ad1337678916fe1d8a
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-01-17 18:34:53 +0200

    changed http-requests to requests without http infrontof in Hangman-game

--- hangman/scripts/requests.js
+++ hangman/scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzle = async(wordCount) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${wordCount}`)
 
     if(response.status === 200) {
         const data = await response.json()
@@ -15,7 +15,7 @@ const getCurrentCountry = async() => {
 }
 
 const getCountryCode = async(countryCode) => {
-    const response = await fetch(`http://restcountries.eu/rest/v2/all`)
+    const response = await fetch(`//restcountries.eu/rest/v2/all`)
 
     if(response.status === 200){
         const data = await response.json()
@@ -26,7 +26,7 @@ const getCountryCode = async(countryCode) => {
 }
 
 const getLocation = async() => {
-    const response = await fetch(`http://ipinfo.io/json?token=3d4e7ffcf7483a`)
+    const response = await fetch(`//ipinfo.io/json?token=3d4e7ffcf7483a`)
 
     if(response.status === 200) {
         const data = await response.json()
