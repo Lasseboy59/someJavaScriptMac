f851e4754edeaaaac58fc589da030dd7389e3149
Lauri Kyttälä <lauri.kyttala@gmail.com>
2019-01-29 15:38:58 +0200

    started recipe-LK-2 directory

---
+++ recipe-LK-2/src/recipes.js
@@ -0,0 +1,111 @@
+// Import needed libraries
+import moment from 'moment';
+import uuidv4 from 'uuid/v4'
+
+// Recipes array
+let recipes = []
+
+// Read existing recipes from the storege
+const loadRecipes = () => {
+    const recipesJSON = localStorage.getItem('recipes')
+
+    try {
+        return recipesJSON !== null ? JSON.parse(recipesJSON) : []
+    } catch(e) {
+        return []
+    }
+}
+
+// Expose recipes from module
+recipes = loadRecipes()
+const getRecipes = () => recipes
+
+const createRecipe = () => {
+    const newRecipe = {
+        id: uuidv4(),
+        title: '',
+        text: '',
+        ingredients: [],
+        createdAt: moment().valueOf(),
+        updatedAt: moment().valueOf()
+    }
+    recipes.push(newRecipe)
+    saveRecipes()
+    return newRecipe
+} 
+
+// Save the recipes to local storage
+const saveRecipes = () => {
+    localStorage.setItem('recipes', JSON.stringify(recipes))
+}
+
+// Remove a recipe from the list
+const removeRecipe = (id) => {
+    const recipeIndex = recipes.findIndex((recipe) => recipe.id === id)
+
+    if(recipeIndex > -1) {
+        recipes.splice(recipeIndex, 1)
+        saveRecipes()
+    }
+}
+
+// Sort your recipes by one of three ways
+const sortRecipes = (sortBy) => {
+    if (sortBy === 'byEdited') {
+        return recipes.sort((a, b) => {
+            if (a.updatedAt > b.updatedAt) {
+                return -1
+            } else if (a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'byCreated') {
+        return recipes.sort((a, b) => {
+            if (a.createdAt > b.createdAt) {
+                return -1
+            } else if (a.createdAt < b.createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if (sortBy === 'alphabetical') {
+        return recipes.sort((a, b) => {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else {
+        return recipes
+    }
+}
+
+const updateRecipe = (id, updates) => {
+    const recipe = recipes.find((recipe) => recipe.id === id)
+
+    if(!recipe) {
+        return
+    }
+
+    if(typeof updates.title === 'string') {
+        recipe.title = updates.title
+        recipe.updatedAt = moment().valueOf()
+    }
+
+    if(typeof updates.text === 'string') {
+        recipe.text = updates.text
+        recipe.updatedAt = moment().valueOf()
+    }
+    saveRecipes()
+    return recipe
+}
+
+export { getRecipes, loadRecipes, createRecipe, removeRecipe, saveRecipes, sortRecipes, updateRecipe }
+
+
